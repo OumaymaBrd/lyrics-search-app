bfa0982023243b0c18ac50a98326afe9b43864e4
OumaymaBrd <oumaymabramid@gmail.com>
2025-04-17 22:54:34 +0100

    Modifier le main

--- src/main.js
+++ src/main.js
@@ -1,4 +1,37 @@
+// src/main.js
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    sets: {
+      mdi,
+    },
+  },
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#1867C0',
+          secondary: '#5CBBF6',
+        },
+      },
+    },
+  },
+})
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
